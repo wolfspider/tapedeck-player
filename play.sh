f3e88e6cd9f5c7476820cc3a038d1ca270784063
wolfspider <jesse.alexander.bennnett@gmail.com>
2021-08-09 01:40:01 -0400

    player script added

---
+++ play.sh
@@ -0,0 +1,15 @@
+#!/usr/local/bin/bash
+
+# TapeDeck Player
+#
+# Usage: play.sh <bosque-source-file>
+# Example:
+# play.sh input.bsq
+
+
+if [[ -z "$1" ]]; then
+    echo "No video file specified"
+    exit 1
+fi
+
+node tapedeck/BosqueLanguage/impl/bin/runtimes/exegen/exegen.js "$1"
